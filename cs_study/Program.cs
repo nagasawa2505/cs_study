2ed03a743b65803960ff8a5d4192138e8f1f4374
DESKTOP-8DV2N22\h250413 <mkhr105052@gmail.com>
2025-05-22 15:16:29 +0900

    C#最初の学習内容

---
+++ cs_study/Program.cs
@@ -0,0 +1,33 @@
+﻿//45ページ　変数の型
+
+//整数型
+int month; //月
+month = 5;
+//変数の宣言と初期化を別で行った
+
+int day = 22; //宣言と初期化を同時に行った
+
+//単精度少数点型
+float temperature_f = 26.2f;
+
+//倍精度小数点型
+double temperature_d = 26.2;
+
+//文字列型
+string week = "木曜日"; //ダブルクォーテーション
+
+//フラグ（論理型）true / false
+bool temp_display = false;
+
+//まとめる文字列
+string announce
+    = month + "月" + day + "日は" + week + "です。";
+
+Console.WriteLine(announce);
+
+//もしもtemp_displayがONなら（trueなら）
+//温度の情報を出す
+if (temp_display == true)
+{
+    Console.WriteLine("温度は" + temperature_f + "度です。");
+}
