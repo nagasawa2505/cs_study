b4c6def9f49073309fc9a78a128bc250778b63de
DESKTOP-8DV2N22\h250413 <mkhr105052@gmail.com>
2025-05-23 15:31:08 +0900

    オブジェクト指向

---
+++ object_study/Program.cs
@@ -0,0 +1,20 @@
+﻿//複数のプレイヤーをたくさん登場させて、
+//それぞれのステータスを除いたり、
+//特技を順番に披露させるプログラム
+
+using object_study;
+
+Player suzuki = new Player();
+suzuki.Name("鈴木");
+suzuki.Attack();
+suzuki.StatusDisplay();
+
+Player tanaka = new Player();
+tanaka.Name("田中");
+tanaka.Attack();
+tanaka.StatusDisplay();
+
+Player yamada = new Player();
+yamada.Name("山田");
+yamada.Attack();
+yamada.StatusDisplay();
