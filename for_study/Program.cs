25f5fe53caf82dba15d4aab9b46a80ad90eef8ec
DESKTOP-8DV2N22\h250413 <mkhr105052@gmail.com>
2025-05-23 14:48:55 +0900

    繰り返しとメソッド

---
+++ for_study/Program.cs
@@ -0,0 +1,67 @@
+﻿//冒険プログラムのステータスの話
+
+float attackPower = 10; //攻撃力10
+
+Console.WriteLine("現在の攻撃力：" + attackPower);
+
+//インクリメント演算子
+attackPower++; //1増やす
+//++attackPower; //これもあり
+
+Console.WriteLine("インクリメント後の攻撃力：" + attackPower);
+
+//デクリメント演算子
+attackPower--; //1減らす
+//--attackPower; //これもあり
+
+Console.WriteLine("デクリメント後の攻撃力：" + attackPower);
+
+//攻撃の係数
+float[] attack_k = { 1, 1.2f, 1.2f, 2.5f, 1 };
+
+Random rand = new Random();
+int result;
+
+for (int i = 0; i < attack_k.Length; i++)
+{
+    result = rand.Next(0, 5);
+    Console.WriteLine("プレイヤーの攻撃：" + attackPower * attack_k[result]);
+}
+
+//おみくじの結果
+string[] omikuji = { "大吉", "吉", "末吉", "凶" };
+
+//result = rand.Next(0, 4);
+//Console.WriteLine("おみくじの結果：" + omikuji[result]);
+
+//while (result != 0)
+//{
+//    result = rand.Next(0, 4);
+//    Console.WriteLine("おみくじの結果：" + omikuji[result]);
+//}
+
+//do
+//{
+//    result = rand.Next(0, 4);
+//    Console.WriteLine("おみくじの結果：" + omikuji[result]);
+//} while (result != 0);
+
+
+//result = 1;
+//while (result != 0)
+//{
+//    result = rand.Next(0, 4);
+//    Console.WriteLine("おみくじの結果：" + omikuji[result]);
+//}
+
+//Console.WriteLine("今年はついてるなあ！");
+
+//for (int i = 0; i < omikuji.Length; i++)
+//{
+//    Console.WriteLine("おみくじの結果：" + omikuji[i]);
+//}
+
+foreach(string omi in omikuji)
+{
+    Console.WriteLine("おみくじの結果：" + omi);
+}
---
+++ method_study/Program.cs
@@ -0,0 +1,17 @@
+﻿Shop(1);
+
+void Shop(int x)
+{
+    if (x == 0)
+    {
+        Console.WriteLine("いらっしゃいませ！");
+    }
+    else if (x == 1)
+    {
+        Console.WriteLine("らっしゃーい");
+    }
+    else
+    {
+        Console.WriteLine("．．．");
+    }
+}
